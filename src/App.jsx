addad63a50361045abb6f8de1c67ccb207c8a26f
demyanchikov <mc.mc.1982@mail.ru>
2023-02-22 12:25:36 +0700

    asd

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+import { useEffect } from 'react';
+
+function App() {
+
+  useEffect( () => {
+    tg.ready();
+  }, []) 
+  
+  return (
+    <div className="App">      
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header/header.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Button from "../Button/Button";
+import './header.css';
+
+const Header = () => {
+    const {user, onClose} = useTelegram();
+
+    return (
+        <div className={'header'}>
+            <Button onClick={onClose}>Закрыть</Button>
+            <span className={'username'}>
+                {user?.username}
+            </span>
+        </div>
+    );
+};
+
+export default Header;
